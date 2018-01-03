e011b9fcf88321a8c32957d3aafbe7c4ffb3c287
Nathan <nchackerian@gmail.com>
2018-01-03 15:01:46 -0600

    leaflet

---
+++ src/mainPage/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = (props) => {
+  return (
+	<div className="navbar">
+      <div className="iconcontainer">
+        <div className="icon"></div>
+        <div className="icon"></div>
+        <div className="icon"></div>
+      </div>
+      <div className="title">Discover Austin!</div>
+    </div>
+  );
+}
+
+export default Nav;
