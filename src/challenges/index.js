015cd0bfa7b40b0b221023fd9bb0d0f5fec7f055
Edward White <ewhite91186@gmail.com>
2018-01-04 15:15:55 -0600

    added path to challenges page

---
+++ src/challenges/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Challenges = () => (
+  <div>
+    <p>challenges page</p>
+  </div>
+);
+
+export default Challenges;
--- src/mainPage/Navbar.js
+++ src/mainPage/Navbar.js
@@ -15,7 +15,8 @@ const Nav = props => (
       <div className="menu">
         <Link to="/"><div className="option" id="link1">Home</div></Link>
         <Link to="/three"><div className="option">Profile</div></Link>
-        <Link to="/two"><div className="option">Leaderboard</div></Link>
+        <Link to="/leaderboard"><div className="option">Leaderboard</div></Link>
+        <Link to="/challenges"><div className="option">Challenges</div></Link>
         <div className="option" id="link3">Logout</div>
       </div> : null}
     </div>
