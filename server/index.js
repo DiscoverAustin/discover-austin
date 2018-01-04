5a347ac4b1a0067003e6fd34001de13e1312e634
Kendrick Gardner <kbgar24@gmail.com>
2018-01-04 11:19:35 -0600

    Install passport, passport-fb, setup client secret

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
-
-const app = express();
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const CLIENT_DIR = path.join(__dirname, '../src/');
+const CLIENT_SECRET = require('./secrets/secrets.js');
+
+const app = express();
 const port = process.env.PORT || 3000;
+const APP_DOMAIN = process.env.DOMAIN || `http://localhost:${3000}`;
+
+passport.use(new FacebookStrategy({
+  clientID: '158163551574274',
+  clientSecret: CLIENT_SECRET,
+  callbackURL: `${APP_DOMAIN}/auth/facebook/callback`,
+}));
 
 app.use(express.static(DIST_DIR));
 app.use(morgan('dev'));
