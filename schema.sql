8423f0b0cb5f1ffbba170ea32a2782dd86c536de
Rick Gomez <richard.p.gomez@gmail.com>
2018-01-06 11:58:54 -0600

    update schema to add yelp_url and user.image_url

--- schema.sql
+++ schema.sql
@@ -8,6 +8,8 @@ CREATE TABLE Users (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(50) NOT NULL,
   email varchar(50) NOT NULL UNIQUE,
+  image_url varchar(250) NOT NULL,
+  total_points int NOT NULL,
   PRIMARY KEY (ID)
 );
 
@@ -22,6 +24,7 @@ CREATE TABLE Achievements (
   category_id int NOT NULL,
   description varchar(50) NOT NULL,
   points int NOT NULL,
+  yelp_url varchar(250) NOT NULL,
   PRIMARY KEY (ID),
   FOREIGN KEY (category_id) REFERENCES categories (id)
 );
@@ -52,20 +55,20 @@ CREATE TABLE Users_Achievements (
 INSERT INTO Categories (id, name) VALUES (1, 'Austin Landmarks');
 INSERT INTO Categories (id, name) VALUES (2, 'Museums');
 
-INSERT INTO Achievements (id, category_id, description, points) VALUES (1, 1, 'State Capitol', 75);
-INSERT INTO Achievements (id, category_id, description, points) VALUES (2, 1, 'Hack Reactor', 75);
-INSERT INTO Achievements (id, category_id, description, points) VALUES (3, 1, 'Barton Springs', 75);
-INSERT INTO Achievements (id, category_id, description, points) VALUES (4, 1, 'Lady Bird Lake', 75);
-INSERT INTO Achievements (id, category_id, description, points) VALUES (5, 1, 'The Driskell', 75);
-INSERT INTO Achievements (id, category_id, description, points) VALUES (6, 1, '6th Street', 75);
-INSERT INTO Achievements (id, category_id, description, points) VALUES (7, 2, 'The Blanton Museum', 75);
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (1, 1, 'State Capitol', 75, 'https://www.yelp.com/biz/texas-state-capitol-austin');
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (2, 1, 'Hack Reactor', 75, 'https://www.yelp.com/biz/hack-reactor-austin-austin');
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (3, 1, 'Barton Springs', 75, 'https://www.yelp.com/biz/barton-springs-pool-austin');
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (4, 1, 'Lady Bird Lake', 75, 'https://www.yelp.com/biz/lady-bird-lake-austin');
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (5, 1, 'The Driskill', 75, 'https://www.yelp.com/biz/the-driskill-in-the-unbound-collection-by-hyatt-austin');
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (6, 1, 'Antone\'s Nightclub', 75, 'https://www.yelp.com/biz/antones-nightclub-austin-6');
+INSERT INTO Achievements (id, category_id, description, points, yelp_url) VALUES (7, 2, 'The Blanton Museum', 75, 'https://www.yelp.com/biz/the-blanton-museum-of-art-austin');
 
-INSERT INTO Users (id, name, email) VALUES (1, 'Mario', 'redoveralls@yahoo.com');
-INSERT INTO Users (id, name, email) VALUES (2, 'Luigi', 'greenoveralls@yahoo.com');
-INSERT INTO Users (id, name, email) VALUES (3, 'Princess', 'pinkdress@yahoo.com');
-INSERT INTO Users (id, name, email) VALUES (4, 'Yoshi', 'stickytongue@yahoo.com');
-INSERT INTO Users (id, name, email) VALUES (5, 'Bowser', 'boomboompow@yahoo.com');
-INSERT INTO Users (id, name, email) VALUES (6, 'Wario', 'yourfutureleader@yahoo.com');
+INSERT INTO Users (id, name, email, image_url, total_points) VALUES (1, 'Mario', 'redoveralls@yahoo.com', 'https://media.nintendo.com/nintendo/bin/UOIekvv9YnMZx4CpweP-v4HDIzmqag9J/HpMzIWmEi7j1uKqpBXziLb6qOVKkgjco.jpg', 250);
+INSERT INTO Users (id, name, email, image_url, total_points) VALUES (2, 'Luigi', 'greenoveralls@yahoo.com', 'https://static.giantbomb.com/uploads/scale_small/9/99864/2389814-nsmbuluigi.png', 450);
+INSERT INTO Users (id, name, email, image_url, total_points) VALUES (3, 'Princess', 'pinkdress@yahoo.com', 'https://gaygeekgab.files.wordpress.com/2015/08/img_6792.jpg?w=396&zoom=2', 550);
+INSERT INTO Users (id, name, email, image_url, total_points) VALUES (4, 'Yoshi', 'stickytongue@yahoo.com', 'http://images5.fanpop.com/image/photos/30400000/Yoshi-snowboard-yoshi-30430714-455-480.png', 600);
+INSERT INTO Users (id, name, email, image_url, total_points) VALUES (5, 'Bowser', 'boomboompow@yahoo.com', 'https://vignette.wikia.nocookie.net/fantendo/images/d/d1/Character-bowser.png/revision/latest/scale-to-width-down/531?cb=20150321152800', 500);
+INSERT INTO Users (id, name, email, image_url, total_points) VALUES (6, 'Wario', 'yourfutureleader@yahoo.com', 'http://images4.fanpop.com/image/photos/23800000/Wario-wario-and-waluigi-23803078-300-294.jpg', 250);
 
 INSERT INTO Users_Achievements (id, user_id, achievement_id, date) VALUES (1, 1, 1, '2017-08-02');
 INSERT INTO Users_Achievements (id, user_id, achievement_id, date) VALUES (2, 2, 1, '2017-08-03');
