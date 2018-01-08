121ef218a29387e28a35f2560899e4dc3d8622f5
Kendrick Gardner <kbgar24@gmail.com>
2018-01-08 01:16:09 -0600

    Feature database (#27)
    * Install passport, passport-fb, setup client secret
    
    * Facebook auth in progress
    
    * add database with sample data
    
    * Updating DB schema
    
    * Update mysql config to autocreate db on npm start
    
    * update schema to add yelp_url and user.image_url
    
    * Refactor mysql to promise-mysql
    
    * update pinkie-promise
    
    * Update README
    Build getUserInfo route
    
    * Update schema to address merge conflicts
    
    * Add pinkie lib directly to package.json
    Update index.js to test CS
    
    * Update lint
    
    * Clean up previous commit

--- server/index.js
+++ server/index.js
@@ -8,12 +8,11 @@ const morgan = require('morgan');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 
-// local files
 const db = require('../db');
 
 /*
-   This block below allows both express-http requests &
-   socket.io socket connections to be simultaneously served:
+This block below allows both express-http requests &
+socket.io socket connections to be simultaneously served:
 */
 const app = express();
 
@@ -25,8 +24,6 @@ socket(io);
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const CLIENT_DIR = path.join(__dirname, '../src/');
-
-console.log('CLIENT_SECRET: ', global.CLIENT_SECRET);
 
 const CLIENT_SECRET = global.CLIENT_SECRET ? global.CLIENT_SECRET : require('./secrets/secrets.js'); // eslint-disable-line
 
