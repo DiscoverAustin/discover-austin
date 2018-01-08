fcc52636f5193ae19190f50c6b6bf936aa6099eb
Edward White <MaybeTed@users.noreply.github.com>
2018-01-08 10:42:45 -0600

    Splash Page (#26)
    * renders data from server
    
    * profile styling and routes
    
    * fixed syntax errors
    
    * clicking name directs user to profile page
    
    * finished feed component for mainpage
    
    * added feed, styled leaderboard, grayed out completed challenges
    
    * added animation to challenges divs
    
    * added splash page
    
    * added some responsive design to splash page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import MainPage from './mainPage';
 import Leaderboard from './leaderboard';
 import Profile from './profile';
 import Challenges from './challenges';
+import Splash from './splashPage';
 import NotFoundPage from './notFoundPage';
 
 const App = () => (
@@ -15,6 +16,7 @@ const App = () => (
         <Route path='/leaderboard' component={ Leaderboard }></Route>
         <Route path='/profile' component={ Profile }></Route>
         <Route path='/challenges' component={ Challenges }></Route>
+        <Route path='/login' component={ Splash }></Route>
         <Route component={ NotFoundPage }></Route>
       </Switch>
     </BrowserRouter>
---
+++ src/splashPage/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default class Splash extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showMenu: false,
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      showMenu: !this.state.showMenu,
+    });
+  }
+
+  render = () => (
+  <div className="splash-main-container">
+    <section className="splash-top-bar">
+
+      <div className="navbar-wrapper">
+        <div className="navbar">
+          <div className="iconcontainer" onClick={this.toggleMenu} >
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </div>
+          <div className="navtitle">Discover Austin!</div>
+        </div>
+        {this.state.showMenu ?
+        <div className="menu">
+          <div className="option" id="link1">Log in</div>
+          <div className="option">Sign up</div>
+        </div> : null}
+      </div>
+
+
+      <div className="splash-top-bar-title">Discover Austin!</div>
+      <ul>
+        <li>Log in</li>
+        <li>Sign up</li>
+      </ul>
+    </section>
+    <section className="splash-banner">
+      <img src="http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/10/People-and-dogs-with-a-view-of-Austin-Texas-downtown-skyline.jpg" />
+      <div>
+        <h1>Hello Austin</h1>
+        <h2>Get ready for adventure</h2>
+        <a href="/login">GET STARTED - IT'S FREE</a>
+      </div>
+    </section>
+  </div>
+  )
+}
+
