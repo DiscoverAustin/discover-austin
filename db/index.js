576885b18ec203f7f295b7e42684d94cc499e9cd
Rick Gomez <richard.p.gomez@gmail.com>
2018-01-09 15:02:14 -0600

    add first set of db calls to be implemented

--- db/index.js
+++ db/index.js
@@ -27,7 +27,59 @@ const getUserInfo = id => (
     })
 );
 
+const getUserAchievements = id => (
+  connection
+    .then(db => db.query(`SELECT * FROM achievements a, users_achievements ua, users u 
+                          WHERE u.id = ${id} AND a.id = ua.achievement_id AND u.id = ua.user_id`))
+    .catch((e) => {
+      console.error('Error retreiving from database!: ', e);
+      throw (e);
+    })
+);
+
+const getLeaderboard = () => (
+  connection
+    .then(db => db.query('SELECT name, total_points FROM users ORDER BY total_points DESC LIMIT 10'))
+    .catch((e) => {
+      console.error('Error retreiving from database!: ', e);
+      throw (e);
+    })
+);
+
+// this one will need to be updated w/ first name / last name parameters once schema is updated
+const addNewUser = (name, email, facebookId, pictureUrl) => (
+  connection
+    .then(db => db.query(`INSERT INTO users (name, email, facebook_id, picture_url, total_points) VALUES (${name}, ${email}, ${facebookId}, ${pictureUrl}, 0)`))
+    .catch((e) => {
+      console.error('Error retreiving from database!: ', e);
+      throw (e);
+    })
+);
+
+const updateUserTotalPoints = points => (
+  connection
+    .then(db => db.query(`UPDATE users SET total_points = ${points}`))
+    .catch((e) => {
+      console.error('Error retreiving from database!: ', e);
+      throw (e);
+    })
+);
+
+// need to also update current point total when adding an achievement
+const addCompletedAchievement = (userId, achievementId) => (
+  connection
+    .then(db => db.query(`INSERT INTO Users_Achievements (user_id, achievement_id, date) VALUES (${userId}, ${achievementId}, now())`))
+    .catch((e) => {
+      console.error('Error retreiving from database!: ', e);
+    })
+);
+
 module.exports = {
   getAllUsers,
   getUserInfo,
+  getLeaderboard,
+  addCompletedAchievement,
+  addNewUser,
+  getUserAchievements,
+  updateUserTotalPoints,
 };
