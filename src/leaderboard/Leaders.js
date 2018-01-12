27849c570e51afa0a832c727fa0fa02ceb30d9e2
Rick Gomez <richard.p.gomez@gmail.com>
2018-01-12 10:52:50 -0600

    add leaderboard and feed itegration from database

--- src/leaderboard/Leaders.js
+++ src/leaderboard/Leaders.js
@@ -7,9 +7,9 @@ const Leaders = props => (
       <p className="leaders">
         {props.index + 1}.
         <Link to={`/profile/${props.leader.name}`}>
-          <span className="user" > { props.leader.name }</span>
+          <span className="user" > { props.leader.first_name } {props.leader.last_name} </span>
         </Link>
-        <span className="score"> { props.leader.score }</span>
+        <span className="score"> { props.leader.total_points }</span>
       </p>
     </div>
   </section>
