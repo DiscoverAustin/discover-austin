db174ab4ed32d4cef16d66cf80fd7a4679d1a4b3
Rick Gomez <richard.p.gomez@gmail.com>
2018-01-15 12:11:09 -0600

    add profile feed (#49)
    * Install passport, passport-fb, setup client secret
    
    * Facebook auth in progress
    
    * add database with sample data
    
    * Updating DB schema
    
    * Update mysql config to autocreate db on npm start
    
    * update schema to add yelp_url and user.image_url
    
    * Refactor mysql to promise-mysql
    
    * update pinkie-promise
    
    * Update README
    Build getUserInfo route
    
    * Update schema to address merge conflicts
    
    * Add pinkie lib directly to package.json
    Update index.js to test CS
    
    * Update lint
    
    * Clean up previous commit
    
    * add first set of db calls to be implemented
    
    * add getFeed and getAllAchievments db calls
    
    * add leaderboard and feed itegration from database
    
    * add dynamically created user profile page
    
    * working on rending page on clicking profile link
    
    * update container styling to profile page
    
    * render challenge page dynamially and adjusted server/db calls accordingly
    
    * render challenge page dynamially and adjusted server/db calls accordingly
    
    * add user feed to profile page

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Feed from '../mainPage/Feed';
 
 export default class Profile extends Component {
   constructor(props) {
     super(props);
-    this.state = { user: this.props.user };
+    this.state = {
+      user: this.props.user,
+      userFeed: [],
+    };
   }
 
   componentDidMount = () => {
     const param = window.location.pathname.split('/')[2];
-    axios.get('http://localhost:3000/api/getUserInfo', { params: { facebookId: param } })
+    axios.get('/api/getUserInfo', { params: { facebookId: param } })
       .then((res) => {
         this.setState({ user: res.data[0] });
+      })
+      .catch((e) => { console.log(e); });
+    axios.get('/api/getUserFeed', { params: { facebookId: param } })
+      .then((res) => {
+        this.setState({ userFeed: res.data });
       })
       .catch((e) => { console.log(e); });
   }
@@ -21,8 +30,9 @@ export default class Profile extends Component {
        <div className="prof">
          <img className="profImage" src={ this.state.user.picture_url }/>
          <h2> { this.state.user.first_name } { this.state.user.last_name } </h2>
-         <h2> <span className="score">{ this.state.user.total_points }</span> points</h2>
+         <h2> <span className="score">{ this.state.user.total_points }</span> points </h2>
        </div>
+      <Feed feed={ this.state.userFeed } />
      </div>
    )
 }
