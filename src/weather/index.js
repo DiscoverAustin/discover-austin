444e9f089dd6fb8fdcfb3daff5d8571ed1da4446
Edward White <ewhite91186@gmail.com>
2018-01-15 11:08:07 -0600

    sidebar and weather widget now always visible, even on scroll down. added minor styling adjustments

--- src/weather/index.js
+++ src/weather/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Weather = () => (
       <section className="weather-container">
-      <a href="https://www.accuweather.com/en/us/austin-tx/78701/weather-forecast/351193" className="aw-widget-legal"></a>
-      <div id="awcc1515901564733" className="aw-widget-current" data-locationkey="" data-unit="f" data-language="en-us" data-useip="true" data-uid="awcc1515901564733"></div>
-    </section>
+        <a href="https://www.accuweather.com/en/us/austin-tx/78701/weather-forecast/351193" className="aw-widget-legal"></a>
+        <div id="awcc1515901564733" className="aw-widget-current" data-locationkey="" data-unit="f" data-language="en-us" data-useip="true" data-uid="awcc1515901564733"></div>
+      </section>
 );
 
 export default Weather;
